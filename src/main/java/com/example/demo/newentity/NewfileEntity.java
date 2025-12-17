ea56be90b427c856b135519ba4c0ccc3813445be
bala-amypo <bala@amypo.in>
2025-12-17 08:16:13 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/newentity/NewfileEntity.java
@@ -32,3 +32,40 @@ public class NewfileEntity {
     @Email(messange="Invalid format")
     private String email;
 }
+
+public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Studententity(String name, int id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    public Studententity() {
+    }
+    
+
+    
+    
+    
+
+}
+
+
+
+
