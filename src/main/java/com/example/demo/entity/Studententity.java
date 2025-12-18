c574e781a22e4b6bb670653276ca452ef4a06007
bala-amypo <bala@amypo.in>
2025-12-18 05:49:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -1,49 +1,41 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
+
 
 public class Studententity {
-    @Id
-    private int id;
+   
     private String name;
-   
-    private LocalDate date;
-    private float cgpa;
-    
-    public Studententity(String name, int id, LocalDate date, float cgpa) {
-        this.name = name;
-        this.id = id;
-        this.date = date;
-        this.cgpa = cgpa;
-    }
-    
-      public Studententity() {
-    }
-   
+    private int id;
+    private String email;
     public String getName() {
         return name;
-    }
-    public void setName(String name) {
-        this.name = name;
     }
     public int getId() {
         return id;
     }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public void setId(int id) {
         this.id = id;
     }
-    public LocalDate getDate() {
-        return date;
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public Studententity(String name, int id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
+    public Studententity() {
     }
     
+
     
+    
+    
+
 }
